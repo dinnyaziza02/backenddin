ea1110a824c62ee59269c9c328550bbd3c8a50a3
Dinny14 <meiayuning49@gmail.com>
2022-07-08 13:34:01 +0700

    Cus

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const url =
   "mongodb+srv://admin:admin@projectuas.hn7e9pg.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(url);
